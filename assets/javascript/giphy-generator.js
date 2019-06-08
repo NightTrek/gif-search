8f1e423fa33999d458f6307f0e1e4ccdb09e3e38
ToneDef00 <daniels@dual4t.com>
2019-06-07 18:15:01 -0700

    working version finished functionality

--- assets/javascript/giphy-generator.js
+++ assets/javascript/giphy-generator.js
@@ -1,43 +1,66 @@
-
 //define global
-
-
 var submit = $('#submit');
 var textInput = $('#text-input');
-var output = $('#gifs');
-
-var linkbuilder = function(string){
-    return `https://api.giphy.com/v1/gifs/search?api_key=7NCgQNu9PIxYnwBdzXkXtF9OLJEuaVd3&q=${string}&limit=1&offset=0&rating=PG&lang=en`
+var gifArray = [];
+var deletionNumber = 0;
+var linkbuilder = function (string) {
+    return `https://api.giphy.com/v1/gifs/search?api_key=7NCgQNu9PIxYnwBdzXkXtF9OLJEuaVd3&q=${string}&limit=25&offset=0&rating=PG&lang=en`
 }
 
-var renderNewGif = function(input){
-    let inputId = input.id
-    let newDiv = $("<div>").attr('class', "col-sm-6 col-lg-3").attr("data", inputId).attr('data-loop', input.images.looping.mp4).attr('data-static', input.images.fixed_height.mp4);
-    let img = $('<img>').attr('src', input.images.fixed_height.url).attr("data", inputId).attr('data-loop', input.images.looping.mp4).attr('data-static', input.images.fixed_height.mp4);
-    newDiv.append(img);
-    output.append(newDiv);
+//GIFs are objects which have a newDiv img and ID they have functions which allow. you can render them to animate when clicked and delete them
+class Gif {
+    constructor(id, string) {
+        var that = this;
+        this.ID = id;
+        this.state = "static";
+        this.newDiv = $('<div>');
+        this.img = $('<img>');
+        this.output = $('#gifs');
+        $.ajax({
+            url: linkbuilder(string),
+            method: "GET"
+        }).then(function (response) {
+            let number = Math.floor(Math.random() * response.data.length)
+            that.static = response.data[number].images.fixed_height_still.url;
+            that.loop = response.data[number].images.fixed_height.url;
+            that.newDiv.attr('class', "col-sm-6 col-lg-3 gif mx-5 my-2").attr("data", that.ID);
+            that.img.attr('src', that.static).attr("data", that.ID).attr('width', '300px');
+            that.newDiv.append(that.img);
+            that.output.prepend(that.newDiv);
+        });
 
+    }
+
+    renderAfterClick() {
+        this.newDiv.empty();
+        if (this.state === 'static') {
+            this.newDiv.append(this.img.attr("src", this.loop));
+            this.state = "loop";
+        }
+        else {
+            this.newDiv.append(this.img.attr("src", this.static));
+            this.state = "static";
+        }
+    }
+    DeleteSelf() {
+        this.newDiv.remove();
+    }
 }
 
-var getGif = function(string){
-    
-    $.ajax({
-        url: linkbuilder(string),
-        method: "GET"
-    }).then(function (response) {
-        console.log(response.data[0]);
-        renderNewGif(response.data[0]);
-    });
-
-}
-
-
-
-$(document).on("click", "#submit", function(){
-    console.log('clicked submit');
+$(document).on("click", "#submit", function () {
     let val = textInput.val();
-    getGif(val);
+    gifArray.push(new Gif(gifArray.length, val));
+    if (gifArray.length > 4) {
+        let removedGif = gifArray.splice(deletionNumber, 1, "false");
+        deletionNumber++;
+        removedGif[0].DeleteSelf();
+    }
 
 
 
+});
+
+$(document).on("click", ".gif", function () {
+    let clickedGif = gifArray[$(this).attr('data')];
+    clickedGif.renderAfterClick();
 });
